ca701f2adcb1f3b9d966c756984a69854ec8bc88
bwuphan <bwuphan@gmail.com>
2019-08-04 11:34:52 -0700

    Update styling and remove Password Forget from src/components/Account/index.js

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 import { AuthUserContext, withAuthorization } from '../Session';
-import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
 
 const AccountPage = () => (
   <AuthUserContext.Consumer>
     {authUser => (
       <div>
-        <h1>Account: {authUser.email}</h1>
-        <PasswordForgetForm />
+        <h1 className="text-center">Account: {authUser.email}</h1>
         <PasswordChangeForm />
       </div>
     )}
