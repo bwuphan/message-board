338d485a45a5f80a23e11d7dabc5740efcf95833
bwuphan <bwuphan@gmail.com>
2019-08-03 23:09:31 -0700

    Add styling to src/components/Navigation/index.js

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,49 +4,65 @@ import { Link } from 'react-router-dom';
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 import { AuthUserContext } from '../Session';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
 
 const Navigation = () => (
-  <div>
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <Link to={ROUTES.LANDING}>
+    <img width="80"
+      src="https://logos-download.com/wp-content/uploads/2016/04/Sacramento_Kings_logo_transparent_bg.png" />
+  </Link>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
     <AuthUserContext.Consumer>
       {authUser =>
         authUser ? <NavigationAuth /> : <NavigationNonAuth />
       }
     </AuthUserContext.Consumer>
-  </div>
+</nav>
 );
 
 const NavigationAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.HOME}>Home</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ACCOUNT}>Account</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ADMIN}>Admin</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.BOARD}>Board</Link>
-    </li>
-    <li>
-      <SignOutButton />
-    </li>
-  </ul>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav mr-auto">
+      <li className="nav-item">
+        <span className="nav-link" href="#">
+          <Link className="text-decoration-none" to={ROUTES.HOME}>Home</Link>
+        </span>
+      </li>
+      <li className="nav-item">
+        <span className="nav-link" href="#">
+          <Link className="text-decoration-none" to={ROUTES.ACCOUNT}>Account</Link>
+        </span>
+      </li>
+      <li className="nav-item">
+        <span className="nav-link" href="#">
+          <Link className="text-decoration-none" to={ROUTES.ADMIN}>Admin</Link>
+        </span>
+      </li>
+      <li className="nav-item">
+        <span className="nav-link" href="#">
+          <Link className="text-decoration-none" to={ROUTES.BOARD}>Board</Link>
+        </span>
+      </li>
+    </ul>
+    <SignOutButton />
+  </div>
+
 );
 
 const NavigationNonAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-    </li>
-  </ul>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav mr-auto">
+    </ul>
+    <div className="nav-item">
+      <span className="nav-link" href="#">
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+      </span>
+    </div>
+  </div>
 );
 
 export default Navigation;
