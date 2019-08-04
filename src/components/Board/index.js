0ad5b5d79c219795ff45bcd50a1b8b397b117b7b
bwuphan <bwuphan@gmail.com>
2019-08-04 11:35:43 -0700

    Update styling in Board

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -3,15 +3,12 @@ import React from 'react';
 import CreateMessage from '../CreateMessage';
 import Messages from '../Messages';
 
-const Board = () => {
-  const condition = authUser => !!authUser;
-  return (
-    <div className="container">
-      <h1 className="text-center">Message Board</h1>
-      <CreateMessage />
-      <Messages />
-    </div>
-  );
-}
+const Board = () => (
+  <div className="container">
+    <h1 className="text-center">Message Board</h1>
+    <CreateMessage />
+    <Messages />
+  </div>
+)
 
 export default Board;
