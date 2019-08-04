6c4f1da0a76fba78cc1a70a8cbe0d37c9b08893e
bwuphan <bwuphan@gmail.com>
2019-08-04 11:36:42 -0700

    Update styling in Message

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import Moment from 'react-moment';
 
-const message = (props) => {
-  var date = new Date(props.message.createdOn);
-  return (
-    <div className="card w-100">
-      <div className="card-body">
-        <h5 className="card-title">{props.user.username}</h5>
-        <p className="card-text">{props.message.text}</p>
-        <h6 className="card-subtitle mb-2 text-muted"><Moment fromNow>{props.message.createdOn}</Moment></h6>
-      </div>
+const message = (props) => (
+  <div className="card w-100 rounded">
+    <div className="card-body">
+      <h5 className="card-title text-primary">{props.user.username}</h5>
+      <p className="card-text">{props.message.text}</p>
+      <h6 className="card-subtitle mb-2 text-muted text-right">
+        Posted <Moment fromNow>{props.message.createdOn}</Moment>
+      </h6>
     </div>
-  );
-};
+  </div>
+);
 
 export default message;
