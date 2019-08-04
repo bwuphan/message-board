c8a5715a9248ea7b8abec7092c23a486c0db19f8
bwuphan <bwuphan@gmail.com>
2019-08-03 23:11:20 -0700

    Update src/components/SignOut/index.js button styling

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -3,7 +3,11 @@ import React from 'react';
 import { withFirebase } from '../Firebase';
 
 const SignOutButton = ({ firebase }) => (
-  <button type="button" onClick={firebase.doSignOut}>
+  <button
+    type="button"
+    className="btn btn-link text-decoration-none pl-0"
+    onClick={firebase.doSignOut}
+  >
     Sign Out
   </button>
 );
