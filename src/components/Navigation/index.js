207e5a0867ab5dd00523dbd64f4f29c651f51ee7
bwuphan <bwuphan@gmail.com>
2019-08-04 11:37:52 -0700

    Fix error messages in Navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,31 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import SignOutButton from '../SignOut';
-import * as ROUTES from '../../constants/routes';
 import { AuthUserContext } from '../Session';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
+import * as ROUTES from '../../constants/routes';
 
 const Navigation = () => (
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <Link to={ROUTES.BOARD}>
-    <img width="80"
-      src="https://logos-download.com/wp-content/uploads/2016/04/Sacramento_Kings_logo_transparent_bg.png" />
-  </Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-    <AuthUserContext.Consumer>
-      {authUser =>
-        authUser ? <NavigationAuth /> : <NavigationNonAuth />
-      }
-    </AuthUserContext.Consumer>
-</nav>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <Link to={ROUTES.BOARD}>
+      <img width="80"
+        src="https://logos-download.com/wp-content/uploads/2016/04/Sacramento_Kings_logo_transparent_bg.png"
+        alt="kings_logo"
+        />
+    </Link>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+      <AuthUserContext.Consumer>
+        {authUser =>
+          authUser ? <NavigationAuth /> : <NavigationNonAuth />
+        }
+      </AuthUserContext.Consumer>
+  </nav>
 );
 
 const NavigationAuth = () => (
   <div className="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav mr-auto">
+    <ul className="navbar-nav mr-auto">
       <li className="nav-item">
         <span className="nav-link" href="#">
           <Link className="text-decoration-none" to={ROUTES.ACCOUNT}>Account</Link>
@@ -45,7 +45,7 @@ const NavigationAuth = () => (
 
 const NavigationNonAuth = () => (
   <div className="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav mr-auto">
+    <ul className="navbar-nav mr-auto">
     </ul>
     <div className="nav-item">
       <span className="nav-link" href="#">
