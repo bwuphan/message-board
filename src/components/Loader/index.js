4bb5d38bd08ec7e7389c288182b03bbc4e8b1fc3
bwuphan <bwuphan@gmail.com>
2019-08-04 11:36:19 -0700

    Add custom color to Loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const loader = () => (
-  <div className="spinner-border" role="status">
-    <span className="sr-only">Loading...</span>
-  </div>
-)
+const loader = ({ color }) => {
+  const colorClass = color ? `text-${color}` : '';
+  return (
+    <div className={`spinner-border ${colorClass}`} role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+};
 
 export default loader;
