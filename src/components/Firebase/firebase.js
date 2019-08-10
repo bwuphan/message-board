09e30eb7508ab30b8beb150ab5e1e570411a800b
bwuphan <bwuphan@gmail.com>
2019-08-10 14:36:46 -0700

    Remove firebase config

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,13 +3,12 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const config = {
-  apiKey: "AIzaSyBJFsr_g7sxkWkWsIjcpXsKp9zkiLMiaks",
-  authDomain: "message-board-5271a.firebaseapp.com",
-  databaseURL: "https://message-board-5271a.firebaseio.com",
-  projectId: "message-board-5271a",
-  storageBucket: "message-board-5271a.appspot.com",
-  messagingSenderId: "115494551486",
-  appId: "1:115494551486:web:a074ec1090c179ec"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 class Firebase {
